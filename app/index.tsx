79758ac2a0c88c2e91ba8f8552219459d3013ebb
minja-ninkovic <88144229+minja-ninkovic@users.noreply.github.com>
2023-06-22 20:13:10 +0200

    looks good for now, fingers crossed

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,19 @@
 import { Link, Stack } from "expo-router";
+import React from "react";
 import { View } from "react-native";
+import { ResizeMode, Video } from "expo-av";
+import * as FileSystem from "expo-file-system";
 
 export default function Root() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Stack.Screen options={{ title: "HOME" }} />
       <Link href="/vision-camera">GO TO VISION CAMERA</Link>
+      <Video
+        useNativeControls={true}
+        resizeMode={ResizeMode.CONTAIN}
+        source={{ uri: `${FileSystem.documentDirectory}output.mp4` }}
+      />
     </View>
   );
 }
--- src/atoms/Camera.tsx
+++ src/atoms/Camera.tsx
@@ -6,7 +6,14 @@ import {
   CameraProps,
   useCameraDevices,
   CameraDevice,
+  useFrameProcessor,
 } from "react-native-vision-camera";
+import {
+  FFmpegKit,
+  FFmpegKitConfig,
+  ReturnCode,
+} from "ffmpeg-kit-react-native";
+import * as FileSystem from "expo-file-system";
 
 interface Props extends Omit<CameraProps, "device" | "isActive"> {
   isActive?: boolean;
@@ -17,10 +24,61 @@ function _Camera({ isActive = true, device, ...props }: Props) {
   const devices = useCameraDevices();
   const _device = device || devices.back;
 
+  const [pipe, setPipe] = React.useState(null);
+  const once = React.useRef(false);
+
+  const frameProcessor = useFrameProcessor((frame) => {
+    "worklet";
+    if (pipe) FFmpegKitConfig.writeToPipe(frame.toString(), pipe);
+  }, []);
+
+  React.useEffect(() => {
+    !pipe &&
+      setPipe(
+        FFmpegKitConfig.registerNewFFmpegPipe().then((_pipe) => {
+          FFmpegKit.executeAsync(
+            `-hide_banner -y -i ${_pipe} -filter_complex loop=loop=-1 -c:v libx264 -r 30 ${FileSystem.documentDirectory}output.mp4`,
+            async (session) => {
+              const state = FFmpegKitConfig.sessionStateToString(
+                await session.getState()
+              );
+              const returnCode = await session.getReturnCode();
+              const failStackTrace = await session.getFailStackTrace();
+
+              // ffprint(`FFmpeg process exited with state ${state} and rc ${returnCode}.${notNull(failStackTrace, "\\n")}`);
+
+              // this.hideProgressDialog();
+
+              // CLOSE PIPES
+              FFmpegKitConfig.closeFFmpegPipe(pipe);
+
+              if (ReturnCode.isSuccess(returnCode)) {
+                // ffprint("Create completed successfully; playing video.");
+                // this.playVideo();
+                // listAllStatistics(session);
+              } else {
+                // ffprint("Create failed. Please check log for the details.");
+              }
+            }
+          );
+        })
+      );
+
+      return () => {
+        FFmpegKit.cancel();
+      }
+  }, [pipe]);
+
   if (_device == null) return null;
 
   return (
-    <DefaultCamera style={StyleSheet.absoluteFill} device={_device} isActive={isActive} {...props} />
+    <DefaultCamera
+      style={StyleSheet.absoluteFill}
+      device={_device}
+      isActive={isActive}
+      {...props}
+      frameProcessor={frameProcessor}
+    />
   );
 }
 
